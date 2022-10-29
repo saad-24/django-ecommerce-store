89f204316f6c247e330017ec71536ad8c863a9b1
saad-24 <75729385+saad-24@users.noreply.github.com>
2022-10-30 03:57:38 +0500

    Add files via upload

---
+++ ecomm_store/urls.py
@@ -0,0 +1,11 @@
+"""ecomm_store URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('store.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
+
+]
